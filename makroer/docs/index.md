21c74a8e9e51b45808915d037786d928a86c289e
Arnfinn Hykkerud Steindal <arnfinn.steindal@skde.no>
2017-08-21 15:34:08 +0200

    [doc] mer tekst i index

--- makroer/docs/index.md
+++ makroer/docs/index.md
@@ -1,3 +1,35 @@
+
+Dokumentasjon av SAS-makroene.
+
+Makroene ligger her:
+```
+\tos-sas-skde-01\SKDE_SAS\Makroer\master
+```
+For å bruke de i din egen SAS-kode, legges følgende inn i koden:
+```
+%let filbane=\tos-sas-skde-01\SKDE_SAS\;
+options sasautos=("&filbane.Makroer\master" SASAUTOS);
+```
+
+Hvis man vil lage en ny makro, lager man en sas-fil som heter det samme som makroen. Makroene dokumenteres direkte i koden. Eksempel på makro, som legges i en fil som heter `minNyeMakro.sas`:
+```
+%macro minNyeMakro(variabel1 = );
+
+/*!
+Min dokumentasjon av makroen minNyeMakro
+*/
+
+sas-kode...
+
+%mend;
+```
+
+
+
+Alt som ligger mellom `/*!` og `*/` vil legges inn i `docs/minNyeMakro.md` av scriptet `lagDokumentasjon.py` i mappen `docs`. Ved å kjøre dette scriptet og så dytte opp til *github*, vil dokumentasjon legges på nett.
+
+## Linker til dokumentasjon av de ulike makroene
+
 - [boomraader](boomraader)
 - [Episode_of_care](Episode_of_care)
 - [Hyppigste](Hyppigste)
--- makroer/docs/lagDokumentasjon.py
+++ makroer/docs/lagDokumentasjon.py
@@ -6,6 +6,10 @@ import os
 import codecs
 
 def extractDoc(filename):
+
+   # Extract text in file that are
+   # between /*! and */
+   
    macroFile = open(filename, "r")
    macroFileContent = macroFile.readlines()
    macroFile.close()
@@ -64,8 +68,43 @@ for i in listofMacros:
       docFile.write(heading + doc)
       docFile.close()
 
+      
+indexHeading="""
+Dokumentasjon av SAS-makroene.
+
+Makroene ligger her:
+```
+\\tos-sas-skde-01\SKDE_SAS\Makroer\master
+```
+For å bruke de i din egen SAS-kode, legges følgende inn i koden:
+```
+%let filbane=\\tos-sas-skde-01\SKDE_SAS\;
+options sasautos=("&filbane.Makroer\master" SASAUTOS);
+```
+
+Hvis man vil lage en ny makro, lager man en sas-fil som heter det samme som makroen. Makroene dokumenteres direkte i koden. Eksempel på makro, som legges i en fil som heter `minNyeMakro.sas`:
+```
+%macro minNyeMakro(variabel1 = );
+
+/*!
+Min dokumentasjon av makroen minNyeMakro
+*/
+
+sas-kode...
+
+%mend;
+```
+
+
+
+Alt som ligger mellom `/*!` og `*/` vil legges inn i `docs/minNyeMakro.md` av scriptet `lagDokumentasjon.py` i mappen `docs`. Ved å kjøre dette scriptet og så dytte opp til *github*, vil dokumentasjon legges på nett.
+
+## Linker til dokumentasjon av de ulike makroene
+
+"""
+
 indexFile = open("index.md", "w")
-indexFile.write(index)
+indexFile.write(indexHeading+index)
 indexFile.close()
 
 
